02cf9727f7aef750809327462246fcf906b49d15
aroussiasse <antoine.roussiasse@berger-levrault.com>
2019-01-18 13:59:15 +0100

    Lol

--- blog/src/Controller/ActorController.php
+++ blog/src/Controller/ActorController.php
@@ -6,46 +6,46 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CategoryController
+class ActorController
 {
-    function getAllCategories() {
-        $xml = simplexml_load_file('category.xml');
-        $categories= [];
-        foreach($xml->children() as $var)
-        {
-          $arr = [];
-          foreach($var->children() as $child)
-          {
-          // echo $child->getName() . ": " . $child . "<br>";
-          $temp = $child->getName();
-          $val = str_replace($temp,'',$child->asXML());
-          $val = str_replace('<>','',$val);
-          $val = str_replace('</>','',$val);
-          $arr[$temp] = $val;
-          }
-          array_push($categories, $arr);
-        }
-        // $categories= [
-        //     ["id" => $var, "name" => ($xml->name)],
-        // ];
-        $jsonResponse = json_encode($categories);
-        // print_r($categories);
-        return new Response($jsonResponse);
+  function getAllActors() {
+    $xml = simplexml_load_file('actor.xml');
+    $actors= [];
+    foreach($xml->children() as $var)
+    {
+      $arr = [];
+      foreach($var->children() as $child)
+      {
+        // echo $child->getName() . ": " . $child . "<br>";
+        $temp = $child->getName();
+        $val = str_replace($temp,'',$child->asXML());
+        $val = str_replace('<>','',$val);
+        $val = str_replace('</>','',$val);
+        $arr[$temp] = $val;
+      }
+      array_push($actors, $arr);
     }
+    // $categories= [
+    //     ["id" => $var, "name" => ($xml->name)],
+    // ];
+    $jsonResponse = json_encode($actors);
+    // print_r($categories);
+    return new Response($jsonResponse);
+  }
 
-    function getCategory($id) {
-        $category= [
-            "id" => $id,
-            "name" => "Catégorie ".$id
-        ];
+  function getCategory($id) {
+    $category= [
+      "id" => $id,
+      "name" => "Catégorie ".$id
+    ];
 
-        $jsonResponse = json_encode($category);
+    $jsonResponse = json_encode($category);
 
-        return new Response($jsonResponse);
-    }
+    return new Response($jsonResponse);
+  }
 
-    function deleteCategory($id){
-        $jsonResponse = json_encode([]);
-        return new Response($jsonResponse);
-    }
+  function deleteCategory($id){
+    $jsonResponse = json_encode([]);
+    return new Response($jsonResponse);
+  }
 }
